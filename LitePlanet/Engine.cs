8daa8120bfc090f57225af848becad1e758df5c0
azathotep <jamessutherland2014@gmail.com>
2013-12-14 23:28:38 +0000

    Add Engine

---
+++ LitePlanet/Engine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LiteEngine.Xna;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using LiteEngine.Input;
+
+namespace LitePlanet
+{
+    class Engine : LiteXnaEngine
+    {
+        protected override void UpdateFrame(GameTime gameTime, XnaKeyboardHandler keyHandler)
+        {
+            if (keyHandler.IsKeyDown(Keys.Escape))
+                Exit();
+        }
+
+        protected override void DrawFrame(GameTime gameTime)
+        {
+            Renderer.Clear(Color.Aqua);
+        }
+    }
+}
