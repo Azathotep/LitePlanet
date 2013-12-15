bbe838d1b75fae7c69da2964d63975d9cf7203d9
azathotep <jamessutherland2014@gmail.com>
2013-12-15 00:41:38 +0000

    Add ship class, implement ship draw

--- LitePlanet/Engine.cs
+++ LitePlanet/Engine.cs
@@ -7,11 +7,25 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using LiteEngine.Input;
+using LitePlanet.Worlds;
+using LitePlanet.Vessels;
 
 namespace LitePlanet
 {
     class Engine : LiteXnaEngine
     {
+        IShip _ship;
+        IPlanet _planet;
+
+        protected override void Initialize()
+        {
+            _ship = new Ship();
+            Renderer.SetDeviceMode(800, 600, true);
+            Renderer.Camera.SetViewField(80, 60);
+            Renderer.Camera.LookAt(new Vector2(0, 0));
+            base.Initialize();
+        }
+
         protected override void UpdateFrame(GameTime gameTime, XnaKeyboardHandler keyHandler)
         {
             if (keyHandler.IsKeyDown(Keys.Escape))
@@ -20,7 +34,10 @@ namespace LitePlanet
 
         protected override void DrawFrame(GameTime gameTime)
         {
-            Renderer.Clear(Color.Aqua);
+            Renderer.Clear(Color.Black);
+            Renderer.BeginDraw();
+            _ship.Draw(Renderer);
+            Renderer.EndDraw();
         }
     }
 }
---
+++ LitePlanet/Vessels/Ship.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using LiteEngine.Rendering;
+using LiteEngine.Textures;
+using LiteEngine.Math;
+
+namespace LitePlanet.Vessels
+{
+    class Ship : IShip
+    {
+        Texture _texture = new Texture("rocketship");
+
+        public Vector2 Position
+        {
+            get 
+            {
+                return new Vector2(0, 0);
+            }
+        }
+
+        public Vector2 Velocity
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public Vector2 Facing
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public float Rotation
+        {
+            get 
+            {
+                return 0;
+            }
+        }
+
+        public void ApplyForwardThrust(float amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ApplyRotateThrust(float amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw(XnaRenderer renderer)
+        {
+            renderer.DrawSprite(_texture, new RectangleF(Position.X, Position.Y, 1f, 1f), Rotation);
+        }
+    }
+}
